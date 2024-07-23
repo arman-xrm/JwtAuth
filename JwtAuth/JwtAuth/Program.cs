5b9080ad6bc6fb4af2d05c4b47ce941252e098e8
arman-xrm <78912532+arman-xrm@users.noreply.github.com>
2024-07-24 00:02:41 +0400

    Update Program.cs

--- JwtAuth/JwtAuth/Program.cs
+++ JwtAuth/JwtAuth/Program.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
-using System.Text;
 using JwtAuth.Services.Integration;
 using JwtAuth.Services.Interface;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
